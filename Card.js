d48db534a6ddc65813b2f69e1fcf784e3b53bba9
Tsageon <sagaetshepo@gmail.com>
2024-10-04 10:34:43 +0200

    Card Gayme

---
+++ Card.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+  let filePath = './Game' + (req.url === '/' ? '/UI.html' : req.url);
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',};
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      res.writeHead(500);
+      res.end('Sorry, an error occurred.');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
